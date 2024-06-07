47f67b061c2f5e48d229d5b53ee9f3753f69aac8
Paresh8890-Jaisinghani <136781047+Paresh8890-Jaisinghani@users.noreply.github.com>
2024-06-07 23:27:44 +0530

    Description page

--- src/components/description.js
+++ src/components/description.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import reaction from './images/reaction.jpg'
-
-import './stylesheet/home.css';
+import work from './images/work.png'
+import score from './images/score.png'
+import intro from './images/intro.png'
+import './stylesheet/description.css';
 
 const Cover = () => {
     return (
@@ -17,47 +18,54 @@ const Cover = () => {
 
 
 <section className="col">
-
-
 <div className="row">
-<div className="col-lg-4 text-center">
-  <img
-    src={reaction}
-    alt="Placeholder"
-    className="bd-placeholder-img rounded-circle"
-    width="140"
-    height="140"
-  />
-  <h2 className="fw-normal">Heading</h2>
-  <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
- 
-</div>
-<div className="col-lg-4 text-center">
-  <img
-    src={reaction}
-    alt="Placeholder"
-    className="bd-placeholder-img rounded-circle"
-    width="140"
-    height="140"
-  />
-  <h2 className="fw-normal">Heading</h2>
-  <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
-  
-</div>
-<div className="col-lg-4 text-center">
-  <img
-   src={reaction}
-    alt="Placeholder"
-    className="bd-placeholder-img rounded-circle"
-    width="140"
-    height="140"
-  />
-  <h2 className="fw-normal">Heading</h2>
-  <p>And lastly this, the third column of representative placeholder content.</p>
- 
-</div>
-</div>
+      <div className="col-lg-4 d-flex flex-column align-items-center  text-center">
+        <img
+          src={score}
+          alt="Placeholder"
+          className="bd-placeholder-img rounded-circle"
+          width="140"
+          height="140"
+        />
+        <h2 className="fw-normal mt-4">About</h2>
+        <p>Test and improve your driving reflexes with our interactive, fun, and engaging reaction time test.</p>
+       
+      </div>
+      <div className="col-lg-4 d-flex flex-column align-items-center text-center">
+        <img
+          src={work}
+          alt="Placeholder"
+          className="bd-placeholder-img rounded-circle"
+          width="140"
+          height="140"
+        />
+        <h2 className="fw-normal mt-4">Working</h2>
+        <p>Measure your reaction time by clicking quickly when a visual signal appears. Track and improve your reflexes.</p>
+       
+      </div>
+      <div className="col-lg-4 d-flex flex-column align-items-center text-center">
+        <img
+          src={intro}
+          alt="Placeholder"
+          className="bd-placeholder-img rounded-circle"
+          width="140"
+          height="140"
+        />
+        <h2 className="fw-normal mt-4">Feedback</h2>
+        <p>Get personalized feedback on your reaction time. Improve your driving reflexes for safer, more responsive driving.</p>
+      </div>
+    </div>
 </section>
+
+
+<footer>
+        <div>
+            <span>Copyright © 2024 All Rights Reserved</span>
+            <span class="link">
+                <a href="http://localhost:3000/">Home</a>
+            </span>
+        </div>
+    </footer>
 </>
       );
 };
