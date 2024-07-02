debb17a29ba5cacd1193af9ff6fe5283b69e7025
Paresh8890-Jaisinghani <136781047+Paresh8890-Jaisinghani@users.noreply.github.com>
2024-07-02 22:29:52 +0530

    lastpageupdated

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,33 +10,21 @@ const Footer = () => {
 				<div className="left">
 					<div className="location">
 						<FaHome size={20} style={{ color: "white", marginRight: "2rem" }} />
-						<p>IIT Hyderabad , India</p>
+						<h5>IIT Hyderabad , India</h5>
 					</div>
 					<div className="Contact">
 						<FaPhone size={20} style={{ color: "white", marginRight: "2rem" }} />
-						<h4>+919376491531</h4>
+						<h5>+919376491531</h5>
 					</div>
 					<div className="Mail">
 						<FaMailBulk size={20} style={{ color: "white", marginRight: "2rem" }} />
-						<h4>pareshjaisinghani882@gmail.com</h4>
+						<h5>pareshjaisinghani882@gmail.com</h5>
 					</div>
 				</div>
-				<div className="right">
+				{/* <div className="right">
 					<p>Hope you are having a  lovely day</p>
 					<p >Created by <span className="color">Paresh</span> || <span className="color">Siddharth</span>|| <span className="color">Lakshya</span></p>
-
-					<div className="social">
-						<a className="linkedin cursor-pointer" href=''>
-							<FaLinkedin href='' size={25} style={{ color: "white", marginRight: "2rem" }} />
-						</a>
-						<a className="insta cursor-pointer " href=''>
-							<FaInstagram href='' size={25} style={{ color: "white", marginRight: "2rem" }} />
-						</a>
-						<a className="insta cursor-pointer " href=''>
-							<FaGithub href='' size={25} style={{ color: "white", marginRight: "2rem" }} />
-						</a>
-					</div>
-				</div>
+				</div> */}
 			</div>
 		</div>
 	)
--- src/components/last.js
+++ src/components/last.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import './stylesheet/last.css'; // Import CSS
-
+import MissionSection from './stylesheet/mission.js';
+import OurCreativeMind from './stylesheet/creative.js';
+import './stylesheet/last.css'
+// import Contact from './stylesheet/message.js';
+import Footer from './footer.js';
 function LastPage() {
-  // Function to handle returning to homepage
-  const returnToHomepage = () => {
-    // Redirect to homepage, change URL as needed
-    window.location.href = '/';
-  };
 
   return (
-    <section className='lastpage'>
-
-    <div className="last">
-      <h1>Thank You for Visiting!</h1>
-      <p>We appreciate your time on our website.</p>
-      <button id="returnBtn" onClick={returnToHomepage}>
-        Return to Homepage
-      </button>
-    </div>
-    </section>
+    <>
+    <nav className="navbar">
+        <h2 className="logo"><a href="/">ReactSafe</a></h2>
+        <input type="checkbox" id="menu-toggler" />
+        <label htmlFor="menu-toggler" id="hamburger-btn">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="24px" height="24px">
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M3 18h18v-2H3v2zm0-5h18V11H3v2zm0-7v2h18V6H3z" />
+          </svg>
+        </label>
+        <ul className="all-links">
+          <li><a href="/">Home</a></li>
+        </ul>
+      </nav>
+     <MissionSection/>
+     <OurCreativeMind/>
+     <Footer/>
+     </>
   );
 }
 
